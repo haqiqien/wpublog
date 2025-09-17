33c11a187a35adbb71dc445ea199343fa68263f8
addien <juyushaqiqi@gmail.com>
2025-09-17 21:07:11 +0700

    mencoba memanggil  di dashboard

--- app/Http/Controllers/PostDashboardController.php
+++ app/Http/Controllers/PostDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostDashboardController extends Controller
@@ -11,7 +12,7 @@ class PostDashboardController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard', ['posts' => Post::latest()->get()]);
     }
 
     /**
