420d5ac50b350ca5a04f0ba3c3b8635445dd736e
addien <juyushaqiqi@gmail.com>
2025-09-19 22:25:33 +0700

    mengubah alert list pada form add post menjadi validation form pada tiap input

--- resources/views/components/posts/create.blade.php
+++ resources/views/components/posts/create.blade.php
@@ -5,7 +5,7 @@
     </div>
 
     {{-- validation errors --}}
-    @if ($errors->any())
+    {{-- @if ($errors->any())
         <div class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
             <svg class="shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
@@ -20,7 +20,7 @@
                 </ul>
             </div>
         </div>
-    @endif
+    @endif --}}
     {{-- end validation errors --}}
 
     <!-- Modal body -->
@@ -28,20 +28,30 @@
         @csrf
         <div class="mb-4">
             <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-            <input type="text" name="title" id="title" class=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type post title name" autofocus value="{{ old('title') }}">
+            <input type="text" name="title" id="title" class="@error('title') border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 border @enderror 
+            border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type post title name" autofocus value="{{ old('title') }}">
+            @error('title')
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oh, snapp!</span> {{ $message }}</p>
+            @enderror
         </div>
         <div class="mb-4">
             <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-            <select name="category_id" id="category" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+            <select name="category_id" id="category" class="@error('category_id') border-red-500 text-red-700 placeholder-red-700 focus:ring-red-500 focus:border-red-500 border @enderror border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
             <option selected="" value="">Select post category</option>
             @foreach (App\Models\Category::get() as $category)
                 <option value="{{ $category->id }}" @selected(old('category_id') == $category->id)>{{ $category->name }}</option>
             @endforeach
             </select>
+            @error('category_id')
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oh, snapp!</span> {{ $message }}</p>
+            @enderror
         </div>
         <div class="sm:col-span-2 mb-4">
             <label for="body" name="body" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body</label>
-            <textarea id="body" name="body" rows="4" class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write post body here">{{ old('body') }}</textarea>
+            <textarea id="body" name="body" rows="4" class="@error('body') border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 border @enderror block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write post body here">{{ old('body') }}</textarea>
+            @error('body')
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oh, snapp!</span> {{ $message }}</p>
+            @enderror
         </div>
         <div class="flex gap-4">
         <button type="submit" class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
