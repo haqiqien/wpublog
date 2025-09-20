3ad5ee0718861faaa67aae85b889f3cac3f1dc5d
addien <juyushaqiqi@gmail.com>
2025-09-20 23:47:57 +0700

    menambahkan validasi gambar

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -56,8 +56,11 @@
         {{-- upload avatar --}}
         <div>
             <label class="block mb-2 text-sm font-medium text-gray-800 dark:text-white" for="avatar">Upload avatar</label>
-            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="avatar_help" id="avatar" name="avatar" type="file">
+            <input class="@error('avatar') border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 border @enderror  block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="avatar_help" id="avatar" name="avatar" type="file" accept=".jpg, .jpeg, .png">
             <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="avatar_help">.png or .jpg</div>
+            @error('avatar')
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oh, snapp!</span> {{ $message }}</p>
+            @enderror
         </div>
 
         <div>
