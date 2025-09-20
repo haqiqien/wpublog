c7ccf4548e41150043b6bfcb98ab113587018f25
addien <juyushaqiqi@gmail.com>
2025-09-20 16:53:46 +0700

    membuat agar form pada halaman edit sudah ada isinya sesuai dengan post yg ingin diupdate

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard/create', [PostDashboardController::class, 'create']);
     Route::delete('/dashboard/{post:slug}', [PostDashboardController::class, 'destroy']);
     Route::get('/dashboard/{post:slug}/edit', [PostDashboardController::class, 'edit']);
+    Route::put('/dashboard/{post:slug}', [PostDashboardController::class, 'update']); 
     Route::get('/dashboard/{post:slug}', [PostDashboardController::class, 'show']);
 });
 
