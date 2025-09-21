3998727cb828bf5a22ec342744e6438eeeb29cc4
addien <juyushaqiqi@gmail.com>
2025-09-22 06:04:15 +0700

    fix profile picture pada show post dashboard

--- resources/views/components/posts/show.blade.php
+++ resources/views/components/posts/show.blade.php
@@ -5,7 +5,7 @@
             <header class="my-4 lg:mb-6 not-format">
                 <address class="flex items-center mb-6 not-italic">
                     <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                        <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
+                        <img class="mr-4 w-16 h-16 rounded-full" src="{{ $post->author->avatar ? asset('storage/' . $post->author->avatar) : asset('img/avatar.png') }}" alt="{{ $post->author->name }}">
                         <div>
                             <a href="/posts?author={{ $post->author->username }}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white hover:underline">{{ $post->author->name }}</a>
                             <a href="/posts?category={{ $post->category->slug }}" class="text-gray-900 block hover:underline"><span class="{{ $post->category->color }} text-grey-800 text-s font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">{{ $post->category->name }}</span></a>
